5e1597f0a64d6dd3cad9b284d26090c418c818f4
Sarchael <mpochec2012@gmail.com>
2020-03-11 20:22:38 +0100

    Initial commit (doesnt work yet because of no datasource configuration)

---
+++ src/main/java/pl/sarchael/zenerius/ZeneriusApplication.java
@@ -0,0 +1,13 @@
+package pl.sarchael.zenerius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeneriusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeneriusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sarchael/zenerius/ZeneriusApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sarchael.zenerius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeneriusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
