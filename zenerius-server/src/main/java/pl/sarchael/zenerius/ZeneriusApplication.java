c74c47cf3e43efb736ffb947a3bf9b08bff2dc13
Sarchael <mpochec2012@gmail.com>
2020-03-14 17:03:56 +0100

    Security vulnerability fix

--- zenerius-server/src/main/java/pl/sarchael/zenerius/ZeneriusApplication.java
+++ zenerius-server/src/main/java/pl/sarchael/zenerius/ZeneriusApplication.java
@@ -9,8 +9,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-@CrossOrigin("*")
 @Configuration
 public class ZeneriusApplication {
 
@@ -23,13 +24,9 @@ public class ZeneriusApplication {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("DELETE");
+        config.addAllowedOrigin("http://localhost:4200");
+        config.setAllowedHeaders(Arrays.asList("AUTHENTICATION_HEADER"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
