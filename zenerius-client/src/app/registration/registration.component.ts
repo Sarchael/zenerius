7d6f3e79cc95c80ccac913b84260c26a78929a8f
przemo029 <przemol1111@o2.pl>
2020-03-22 15:01:58 +0100

    fields validation with usage of FormGroup

--- zenerius-client/src/app/registration/registration.component.ts
+++ zenerius-client/src/app/registration/registration.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SignUpForm } from '../_models/signUpForm';
 import { UserService } from '../services/user.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -10,36 +12,57 @@ import { UserService } from '../services/user.service';
 export class RegistrationComponent implements OnInit {
 
   signUpForm: SignUpForm;
+  registerForm: FormGroup;
+  submitted = false;
   password: string;
   password2: string;
   gender: string;
-  errMessage: string;
-  show: boolean;
+  error = '';
+  returnUrl = '';
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, 
+    private formBuilder: FormBuilder, 
+    private route: ActivatedRoute, 
+    private router: Router) { }
 
   ngOnInit(): void {
     this.signUpForm = new SignUpForm();
-    this.show = false;
+
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+    this.registerForm = this.formBuilder.group({
+      login: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      password2: ['', Validators.required],
+      gender: ['', Validators.required]
+    });
   }
 
-  onSubmit(){
-    if(this.password === this.password2){
-      if(this.signUpForm.gender==="M" || this.signUpForm.gender==="K"){
-        this.signUpForm.password = this.password;
-        this.userService.save(this.signUpForm).subscribe(data => {
-          this.show = false;
-        }, err=> {
-          this.show = true;
-          this.errMessage = "Nieudane logowanie";
-        })
-      } else {
-        this.show = true;
-        this.errMessage = "Wybierz płeć";
-      }
+  mapFormGroupToSingUpForm() {
+    this.signUpForm.login = this.f.login.value;
+    this.signUpForm.password = this.f.password.value;
+    this.signUpForm.email = this.f.email.value;
+    this.signUpForm.gender = this.f.gender.value;
+  }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+    if (this.f.password.value === this.f.password2.value) {
+      this.mapFormGroupToSingUpForm();
+      this.error = '';
+      this.userService.save(this.signUpForm).subscribe(data => {
+        this.router.navigate([this.returnUrl]);
+      }, err => {
+        this.error = "Nieudana rejestracja";
+      })
     } else {
-      this.show = true;
-      this.errMessage = "Hasła muszą być identyczne"
+      this.error = "Hasła muszą być identyczne";
     }
 
   }
